809d79e0c80e8fa9bdf3d5a8fc70366ed42cab22
Darkzero7 <chaiyanituporn@gmail.com>
2024-07-05 15:25:10 +0700

    js lab 166

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// write code here
+// ให้ใช้ Object destructuring โดยให้ a เท่ากับ 5 และ b เท่ากับ 10
+
+const obj = { prop: 5, prop2: 10 };
+const { prop: a, prop2: b } = obj;
+console.log(`a = ${a}, b = ${b}`);
